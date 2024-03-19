7a107e3384384b668708c14abb263f6ff553549f
shrenikm <shrenik95@gmail.com>
2024-03-19 01:27:50 -0700

    done

--- algorithms/zmp/tests/test_footstep_planners.py
+++ algorithms/zmp/tests/test_footstep_planners.py
@@ -41,7 +41,7 @@ def test_footstep_type() -> None:
 def test_naive_footstep_planner(
     left_foot_polygon: PolygonArray,
     right_foot_polygon: PolygonArray,
-    debug: bool = True,
+    debug: bool = False,
 ) -> None:
 
     nfp = NaiveFootstepPlanner(
@@ -52,10 +52,12 @@ def test_naive_footstep_planner(
         right_foot_polygon=right_foot_polygon,
     )
 
+    # Testing cop trajectory generation for different kinds of paths.
+
+    # Straight path in positive x.
     straight_x_path = np.arange(0.0, 10.0, 0.05)
     straight_y_path = np.zeros(straight_x_path.size, dtype=np.float64)
     straight_xy_path = np.vstack((straight_x_path, straight_y_path)).T
-
     cop_trajectory = nfp.plan_cop_trajectory(
         xy_path=straight_xy_path,
         stride_length_m=0.5,
@@ -67,6 +69,46 @@ def test_naive_footstep_planner(
     np.testing.assert_array_almost_equal(
         cop_trajectory.value(cop_trajectory.end_time()).reshape(3),
         np.array([9.95, -0.25, 0.]),
+        decimal=6,
+    )
+
+    # Diagonal path angled 45 degrees.
+    diagonal_x_path = np.arange(0.0, 10.0, 0.05)
+    diagonal_y_path = np.arange(0.0, 10., 0.05)
+    diagonal_xy_path = np.vstack((diagonal_x_path, diagonal_y_path)).T
+    cop_trajectory = nfp.plan_cop_trajectory(
+        xy_path=diagonal_xy_path,
+        stride_length_m=0.5,
+        swing_phase_time_s=1.0,
+        stance_phase_time_s=1.0,
+        first_footstep=FootstepType.RIGHT,
+        debug=debug,
+    )
+    np.testing.assert_array_almost_equal(
+        cop_trajectory.value(cop_trajectory.end_time()).reshape(3),
+        np.array([9.7732233, 10.1267767, 0.78539816]),
+        decimal=6,
+    )
+
+    # Path that turns left 90 degrees.
+    turn_x_path1 = np.arange(0.0, 10.0, 0.05)
+    turn_x_path2 = np.full_like(turn_x_path1, fill_value=10.)
+    turn_x_path = np.hstack((turn_x_path1, turn_x_path2))
+    turn_y_path1 = np.full_like(turn_x_path1, fill_value=0.)
+    turn_y_path2 = np.arange(0.0, 10., 0.05)
+    turn_y_path = np.hstack((turn_y_path1, turn_y_path2))
+    turn_xy_path = np.vstack((turn_x_path, turn_y_path)).T
+    cop_trajectory = nfp.plan_cop_trajectory(
+        xy_path=turn_xy_path,
+        stride_length_m=0.5,
+        swing_phase_time_s=1.0,
+        stance_phase_time_s=1.0,
+        first_footstep=FootstepType.RIGHT,
+        debug=debug,
+    )
+    np.testing.assert_array_almost_equal(
+        cop_trajectory.value(cop_trajectory.end_time()).reshape(3),
+        np.array([10.25, 9.95, 1.570796]),
         decimal=6,
     )
 
